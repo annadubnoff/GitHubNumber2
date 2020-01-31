81f30fabd6106bde7b97c3390a44a84df43ed078
Anna Dubnoff <annadubnoff@gmail.com>
2020-01-31 14:03:29 -0500

    Deleted Player 2

---
+++ GitHubNumber2/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  GitHubNumber2
+//
+//  Created by Anna Dubnoff (student LM) on 1/31/20.
+//  Copyright © 2020 Anna Dubnoff (student LM). All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        let player1 = SKSpriteNode(imageNamed: "Player1")
+        player1.setScale(0.5)
+        player1.position = CGPoint(x: 0, y: -100)
+        player1.zPosition = 1.0
+        addChild(player1)
+        
+
+    }
+}
