8fb632ee77f2b6e8195803e6fd02035057c81fdb
Anna Dubnoff <annadubnoff@gmail.com>
2020-01-31 14:23:39 -0500

    Create Player Two

--- GitHubNumber2/GameScene.swift
+++ GitHubNumber2/GameScene.swift
@@ -20,6 +20,10 @@ class GameScene: SKScene {
         player1.zPosition = 1.0
         addChild(player1)
         
-
+        let player2 = SKSpriteNode(imageNamed: "Player2")
+        player2.setScale(0.5)
+        player2.position = CGPoint(x: 0, y: 100)
+        player2.zPosition = 1.0
+        addChild(player2)
     }
 }
